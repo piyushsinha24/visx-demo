0dd741d3b10ddfe2fc15dd961d179a8ce37ccc05
Piyush Sinha <piyush.sinha24@gmail.com>
2021-03-22 10:20:22 +0530

    total data & sidebyside layout

--- src/Graph.js
+++ src/Graph.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import './Graph.css';
 import {
     AnimatedAxis,
     AnimatedGrid,
+    AnimatedLineSeries,
     AnimatedAreaSeries,
     XYChart,
     Tooltip,
@@ -9,56 +11,110 @@ import {
 
 import CustomChartBackground from './CustomChartBackground';
 
-  const AssociateCount = [
-    { x: "Oct'20", y: 0 },
-    { x: "Nov'20", y: 4 },
-    { x: "Dec'20", y: 6 },
-    { x: "Jan'21", y: 8 },
-    { x: "Feb'21", y: 12 },
-    { x: "Mar'21", y: 16 },
+  const OnboardedCount = [
+    { x: "Oct'20", y: 0, total: 0 },
+    { x: "Nov'20", y: 2, total: 2 },
+    { x: "Dec'20", y: 4, total: 6 },
+    { x: "Jan'21", y: 3, total: 9 },
+    { x: "Feb'21", y: 4, total: 13 },
+    { x: "Mar'21", y: 3, total: 16 },
   ];
   
-  const SelectedCount = [
-    { x: "Oct'20", y: 0 },
-    { x: "Nov'20", y: 1 },
-    { x: "Dec'20", y: 2 },
-    { x: "Jan'21", y: 4 },
-    { x: "Feb'21", y: 6 },
-    { x: "Mar'21", y: 8 },
+  const DeployedCount = [
+    { x: "Oct'20", y: 0, total: 0 },
+    { x: "Nov'20", y: 1, total: 1 },
+    { x: "Dec'20", y: 2, total: 3 },
+    { x: "Jan'21", y: 2, total: 5 },
+    { x: "Feb'21", y: 3, total: 8 },
+    { x: "Mar'21", y: 0, total: 8 },
   ];
   
   const accessors = {
     xAccessor: d => d.x,
     yAccessor: d => d.y,
+    totalAccessor: d => d.total
   };
+
+  const colors = {"Onboarded Count":"#0e8ff9", "Deployed Count": "#ff6200"};
   
   
   class Graph extends Component {
       render() {
           return (
-            <XYChart height={600} xScale={{ type: 'band' }} yScale={{ type: 'linear' }}>
+            <div>
+              <XYChart height={600} xScale={{ type: 'band' }} yScale={{ type: 'linear' }}>
             <CustomChartBackground />
             <AnimatedAxis orientation="bottom"/>
+            <AnimatedAxis orientation="left"/>
             <AnimatedGrid columns={false} numTicks={4} />
-            <AnimatedAreaSeries dataKey="Associate Count" data={AssociateCount} {...accessors} fillOpacity={0.6}/>
-            <AnimatedAreaSeries dataKey="Selected in CI" data={SelectedCount} {...accessors} fillOpacity={0.6}/>
+            <AnimatedLineSeries dataKey="Onboarded Count" data={OnboardedCount} {...accessors} stroke={"#0e8ff9"}/>
+            <AnimatedLineSeries dataKey="Deployed Count" data={DeployedCount} {...accessors} stroke={"#ff6200"} fill={"#ff6200"}/>
             <Tooltip
               snapTooltipToDatumX
               snapTooltipToDatumY
               showVerticalCrosshair
-              showSeriesGlyphs
-              renderTooltip={({ tooltipData, colorScale }) => (
-                <div>
-                  <div style={{ color: colorScale(tooltipData.nearestDatum.key) }}>
+              renderTooltip={({ tooltipData }) => (
+                <div style={{ padding: "4px"}}>
+                  <div style={{ marginBottom: "8px", color: colors[tooltipData.nearestDatum.key]  ,textDecoration: "underline" }}>
                     {tooltipData.nearestDatum.key}
                   </div>
                   {accessors.xAccessor(tooltipData.nearestDatum.datum)}
-                  {', '}
+                  {": "}
                   {accessors.yAccessor(tooltipData.nearestDatum.datum)}
+                  <p>Total: {accessors.totalAccessor(tooltipData.nearestDatum.datum)}</p>
                 </div>
               )}
             />
           </XYChart>
+          <div className="sidebyside">
+          <XYChart height={300} xScale={{ type: 'band' }} yScale={{ type: 'linear' }}>
+            <CustomChartBackground />
+            <AnimatedAxis orientation="bottom"/>
+            <AnimatedAxis orientation="left"/>
+            <AnimatedGrid columns={false} numTicks={4} />
+            <AnimatedAreaSeries dataKey="Onboarded Count" data={OnboardedCount} {...accessors} stroke={"#0e8ff9"} fill={"#0e8ff9"} fillOpacity={0.6}/>
+            <Tooltip
+              snapTooltipToDatumX
+              snapTooltipToDatumY
+              showVerticalCrosshair
+              renderTooltip={({ tooltipData }) => (
+                <div style={{ padding: "4px"}}>
+                  <div style={{ marginBottom: "8px", color: colors[tooltipData.nearestDatum.key]  ,textDecoration: "underline" }}>
+                    {tooltipData.nearestDatum.key}
+                  </div>
+                  {accessors.xAccessor(tooltipData.nearestDatum.datum)}
+                  {": "}
+                  {accessors.yAccessor(tooltipData.nearestDatum.datum)}
+                  <p>Total: {accessors.totalAccessor(tooltipData.nearestDatum.datum)}</p>
+                </div>
+              )}
+            />
+          </XYChart>
+          <XYChart height={300} xScale={{ type: 'band' }} yScale={{ type: 'linear' }}>
+            <CustomChartBackground />
+            <AnimatedAxis orientation="bottom"/>
+            <AnimatedAxis orientation="left"/>
+            <AnimatedGrid columns={false} numTicks={4} />
+            <AnimatedLineSeries dataKey="Deployed Count" data={DeployedCount} {...accessors} stroke={"#ff6200"} fill={"#ff6200"} fillOpacity={0.6}/>
+            <Tooltip
+              snapTooltipToDatumX
+              snapTooltipToDatumY
+              showVerticalCrosshair
+              renderTooltip={({ tooltipData }) => (
+                <div style={{ padding: "4px"}}>
+                  <div style={{ marginBottom: "8px", color: colors[tooltipData.nearestDatum.key]  ,textDecoration: "underline" }}>
+                    {tooltipData.nearestDatum.key}
+                  </div>
+                  {accessors.xAccessor(tooltipData.nearestDatum.datum)}
+                  {": "}
+                  {accessors.yAccessor(tooltipData.nearestDatum.datum)}
+                  <p>Total: {accessors.totalAccessor(tooltipData.nearestDatum.datum)}</p>
+                </div>
+              )}
+            />
+          </XYChart>
+          </div>
+            </div>
           )
       }
   }
